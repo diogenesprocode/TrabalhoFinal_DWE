82e2ce8cd052c2f3b2cd7375e067994257527f2b
Diógenes Oliveira <diogenes_person@outlook.com>
2015-10-13 23:58:03 -0300

    10-13 Atualização na documentação
    Modificação nos documentos relativos a estrutura do banco de dados.

--- Documentacao/esquema_fisico_BD.sql
+++ Documentacao/esquema_fisico_BD.sql
@@ -1,7 +1,14 @@
-CREATE TABLE Livro (
-	Autor varchar(30),
-	Título varchar(30),
-	Tombo int,
-	Id varchar(11),
-	PRIMARY KEY(Tombo,Id)
-)
+CREATE TABLE Estante (
+idEstante int PRIMARY KEY,
+numeroLivros int
+);
+
+CREATE TABLE livroEtiquetado (
+idEtiqueta varchar(11),
+tombo int,
+idEstante int,
+autor varchar(30),
+titulo varchar(30),
+PRIMARY KEY(IdEtiqueta,Tombo),
+FOREIGN KEY(idEstante) REFERENCES estante (idEstante)
+);
---
+++ Páginas/php/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>RFDIBiblioteca - Início</title>
+</head>
+<body>
+	<nav>
+		<a href="index.php">Início</a>
+		<a href="cadastro.php">Cadastro</a>
+		<a href=""></a>
+	</nav>
+</body>
+</html>
